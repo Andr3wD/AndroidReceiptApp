9a684d3f153608bfad3db5832b2bf39408fcfefc
NTCalkins <nicholastcalkins@gmail.com>
2021-04-16 22:44:50 -0700

    started adding content from chapter 16

--- app/src/main/java/com/bignerdranch/android/androidreceiptapp/Receipt.kt
+++ app/src/main/java/com/bignerdranch/android/androidreceiptapp/Receipt.kt
@@ -2,10 +2,14 @@ package com.bignerdranch.android.androidreceiptapp
 
 import java.util.*
 
-data class Receipt ( var ReceiptID: UUID = UUID.randomUUID(),
+@Entity
+data class Receipt ( @PrimaryKey var ReceiptID: UUID = UUID.randomUUID(),
                      var Title: String = "Test Title",
                      var Date : Date = Date(),
                      var TotalSpent: Number = 0.0,
                      var Store: String = "Test Store",
                      var Entries: ArrayList<ReceiptEntry> = ArrayList<ReceiptEntry>()
-)
+) {
+    val photoFileName
+        get() = "IMG_$ReceiptID.jpg"
+}
