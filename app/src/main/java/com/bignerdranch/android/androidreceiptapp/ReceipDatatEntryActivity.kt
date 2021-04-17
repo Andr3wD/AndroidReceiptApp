03d9abb44552b9b5a3bb201bf3bc3790dfd30db8
NTCalkins <nicholastcalkins@gmail.com>
2021-04-16 23:42:52 -0700

    worked on receipt data entry layout

---
+++ app/src/main/java/com/bignerdranch/android/androidreceiptapp/ReceipDatatEntryActivity.kt
@@ -0,0 +1,10 @@
+package com.bignerdranch.android.androidreceiptapp
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class ReceipDatatEntryActivity: AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_data_entry)
+}
