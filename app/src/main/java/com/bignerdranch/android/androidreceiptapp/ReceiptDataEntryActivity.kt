417b9ad958bc483346921da21baf665e19e284b2
Andr3wD <37052064+Andr3wD@users.noreply.github.com>
2021-04-21 16:58:48 -0700

    Working on image cropping

--- app/src/main/java/com/bignerdranch/android/androidreceiptapp/ReceiptDataEntryActivity.kt
+++ app/src/main/java/com/bignerdranch/android/androidreceiptapp/ReceiptDataEntryActivity.kt
@@ -1,21 +1,18 @@
 package com.bignerdranch.android.androidreceiptapp
 
 import android.Manifest
-import android.app.Activity
 import android.content.ActivityNotFoundException
 import android.content.Context
 import android.content.Intent
 import android.content.pm.PackageManager
 import android.graphics.Bitmap
 import android.graphics.BitmapFactory
-import android.graphics.Color
 import android.graphics.ImageFormat
 import android.hardware.camera2.CameraCaptureSession
 import android.hardware.camera2.CameraDevice
 import android.hardware.camera2.CameraManager
 import android.media.ImageReader
 import android.os.Bundle
-import android.os.Handler
 import android.provider.MediaStore
 import android.util.Log
 import android.view.*
@@ -29,15 +26,9 @@ import org.opencv.imgproc.Imgproc
 import java.nio.ByteBuffer
 
 class ReceiptDataEntryActivity : Fragment() {
-//    override fun onCreate(savedInstanceState: Bundle?) {
-//        super.onCreate(savedInstanceState)
-//        setContentView(R.layout.activity_data_entry)
-//    }
 
     private lateinit var cameraButton: ImageButton
     private lateinit var cameraImageDisplay: ImageView
-    private lateinit var surfView: SurfaceView
-    private val REQUEST_IMAGE_CAPTURE = 1
     lateinit var camManager: CameraManager
     lateinit var imgReader: ImageReader
 
@@ -48,65 +39,33 @@ class ReceiptDataEntryActivity : Fragment() {
 
         cameraButton = view.findViewById(R.id.receipt_camera)
         cameraImageDisplay = view.findViewById(R.id.receipt_camera_image)
-        cameraImageDisplay.rotation = 90.0F
+        cameraImageDisplay.rotation = 90F
+        // Using JPEG instead of YUV_420_888 because https://stackoverflow.com/questions/28430024/convert-android-media-image-yuv-420-888-to-bitmap?answertab=votes#tab-top. Faster and easier.
         imgReader = ImageReader.newInstance(500,500,ImageFormat.JPEG, 10)
-        imgReader.setOnImageAvailableListener(ImageReader.OnImageAvailableListener {
+        imgReader.setOnImageAvailableListener({
 
+            // Thanks to https://stackoverflow.com/questions/26673127/android-imagereader-acquirelatestimage-returns-invalid-jpg
             val latestImage = it.acquireLatestImage()
-            val bb: ByteBuffer = latestImage.planes[0].buffer
-            bb.rewind()
-            val ba = ByteArray(bb.remaining())
-            bb.get(ba)
-            val bit: Bitmap = BitmapFactory.decodeByteArray(ba, 0, ba.size)
-            latestImage.close()
-            val contourBit = getContours(bit)
+            val byteBuffer: ByteBuffer = latestImage.planes[0].buffer
+            byteBuffer.rewind()
+            val byteArray = ByteArray(byteBuffer.remaining())
+            byteBuffer.get(byteArray)
+            val bit: Bitmap = BitmapFactory.decodeByteArray(byteArray, 0, byteArray.size)
+            latestImage.close() // Close the image asap so the ImageReader buffer doesn't get full.
 
+            val contourBit = getContours(bit) // Get a bitmap with the contours applied to it.
             cameraImageDisplay.setImageBitmap(contourBit)
 
-
         }, null)
+
+        // Make a camera2 camera attached to the ImageReader surface
         makeCameraStuff(imgReader.surface)
-
-//        surfView = view.findViewById(R.id.receipt_camera_image)
-        cameraButton.setOnClickListener {
-            val cameraIntent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
-            Log.d("Receipt_Entry", "Trying camera intent")
-            try {
-                startActivityForResult(cameraIntent, REQUEST_IMAGE_CAPTURE)
-                Log.d("Receipt_Entry", "Intent completed")
-            } catch (e: ActivityNotFoundException) {
-                Log.e("Receipt_Entry", "Failed to find camera activity!")
-            }
-        }
-
-//        surfView.holder.addCallback(object: SurfaceHolder.Callback {
-//            override fun surfaceCreated(holder: SurfaceHolder) {
-//                Log.d("test", "Surface Created!")
-//                holder.setFixedSize(500,500)
-//                makeCameraStuff(holder)
-//            }
-//
-//            override fun surfaceChanged(
-//                holder: SurfaceHolder,
-//                format: Int,
-//                width: Int,
-//                height: Int
-//            ) {
-//                Log.d("test", "SURFACE CHANGED")
-//            }
-//
-//            override fun surfaceDestroyed(holder: SurfaceHolder) {
-//                Log.d("test", "SURFACE DESTROYED!")
-//            }
-//
-//        })
-
-
 
         return view
     }
 
     private fun makeCameraStuff(surf: Surface) {
+        // https://medium.com/androiddevelopers/understanding-android-camera-capture-sessions-and-requests-4e54d9150295
         camManager = context?.getSystemService(Context.CAMERA_SERVICE) as CameraManager
 
         if (ActivityCompat.checkSelfPermission(context!!, Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
@@ -126,13 +85,16 @@ class ReceiptDataEntryActivity : Fragment() {
             override fun onOpened(camera: CameraDevice) {
                 Log.d("test","CAMERA OPEN")
                 var targets: MutableList<Surface> = arrayOf(surf).toMutableList()
-                // Deprecated, but next 'available' is version 28, so...
 
+                // Deprecated, but next 'available' is version 28, so...
+                // Create a capture session
                 camera.createCaptureSession(targets, object: CameraCaptureSession.StateCallback() {
                     override fun onConfigured(session: CameraCaptureSession) {
+                        // Create a request for capture
                         val capture = session.device.createCaptureRequest(CameraDevice.TEMPLATE_PREVIEW)
                         capture.addTarget(surf)
 
+                        // Make the request repeat
                         session.setRepeatingRequest(capture.build(), null, null)
                         Log.d("test","CAMERA CONFIG SUCCESSFUL")
                     }
@@ -159,134 +121,90 @@ class ReceiptDataEntryActivity : Fragment() {
         // TODO start new fragment with 4 point transform.
         // https://www.pyimagesearch.com/2014/09/01/build-kick-ass-mobile-document-scanner-just-5-minutes/
         // Don't trust immutable yet...
+        // TODO switch to vals eventually.
         // TODO compress into 1 file that constantly replaces itself
         var testMat: Mat = Mat()
         var testGreyMat: Mat = Mat()
         var testGreyGaussianMat: Mat = Mat()
         var testGreyGaussianEdgeMat: Mat = Mat()
 
+        // Convert image bitmap to OpenCV Mat
         Utils.bitmapToMat(imageBitmap, testMat)
-        // Greyscale
+
+        // Make image greyscale
         Imgproc.cvtColor(testMat, testGreyMat, Imgproc.COLOR_BGR2GRAY)
+        // Blur the image
         Imgproc.GaussianBlur(testGreyMat, testGreyGaussianMat, Size(5.0, 5.0), 0.0)
+        // Apply edge detection
         Imgproc.Canny(testGreyGaussianMat, testGreyGaussianEdgeMat, 75.0, 75.0*3)
 
-
-        var matPointList: MutableList<MatOfPoint> = ArrayList()
-        var contourHierarchyMat = Mat()
+        var matPointList: MutableList<MatOfPoint> = ArrayList() // The points that will be found by the findContours() method
+        var contourHierarchyMat = Mat() // No clue what this does.
         // See https://docs.opencv.org/3.4/d4/d73/tutorial_py_contours_begin.html
         Imgproc.findContours(testGreyGaussianEdgeMat, matPointList, contourHierarchyMat, Imgproc.RETR_EXTERNAL, Imgproc.CHAIN_APPROX_SIMPLE)
 
         // Convert to MatOfPoint2f https://stackoverflow.com/questions/11273588/how-to-convert-matofpoint-to-matofpoint2f-in-opencv-java-api
-        var newList: MutableList<MatOfPoint2f> = ArrayList()
+        // Convert to MatOfPoint2f for some reason
+        var newPointList: MutableList<MatOfPoint2f> = ArrayList()
         for (v in matPointList) {
             val d = MatOfPoint2f()
             v.convertTo(d, CvType.CV_32F)
-            newList.add(d)
+            newPointList.add(d)
         }
+        // Sort the point list by the area the contour surrounds.
+        newPointList.sortByDescending { v -> Imgproc.contourArea(v) }
 
-        newList.sortByDescending { v -> Imgproc.contourArea(v) }
-
-        var newoneList: MutableList<MatOfPoint> = ArrayList()
-//            val d = MatOfPoint()
-//            newList.get(0).convertTo(d, CvType.CV_32S)
-//            newoneList.add(d)
-        for (v in newList) {
+        var singlePointList: MutableList<MatOfPoint> = ArrayList()
+        // Parse through points found
+        for (v in newPointList) {
             val p = Imgproc.arcLength(v, true)
-            val ap = MatOfPoint2f()
+            val ap = MatOfPoint2f() // Approximate
             Imgproc.approxPolyDP(v, ap, 0.02 * p, true)
             Log.d("test", ap.size().toString())
-            if (ap.height() == 4) {
+
+            if (ap.height() == 4) { // If 4 points exist, then it's a square/rectangle, and is what we're looking for.
+                // Sidenote: I'm about 99% sure that trying to transform anything that isn't a 4 points is really hard.
+
                 val d = MatOfPoint()
-                ap.convertTo(d, CvType.CV_32S)
-                newoneList.add(d)
+                ap.convertTo(d, CvType.CV_32S) // Convert back to CvType.CV_32S from CvType.CV_32F
+                singlePointList.add(d)
+                //testMat = cropWithContour(testMat, d) This is what I'm having problems with, the actual cropping.
                 break
             }
         }
 
 
-//        Log.d("test", newoneList[0].toString())
+        // Draw the found rectangle/square on the original image Mat
+        Imgproc.drawContours(testMat, singlePointList, -1, Scalar(0.0, 0.0, 255.0), 3)
 
-        Imgproc.drawContours(testMat, newoneList, -1, Scalar(0.0, 0.0, 255.0), 3)
-
-
+        // Convert the original image Mat to a BitMap
         var testBit: Bitmap = Bitmap.createBitmap(imageBitmap.width, imageBitmap.height, imageBitmap.config)
         Utils.matToBitmap(testMat, testBit)
-//            cameraImageDisplay.setImageBitmap(testBit)
 
         return testBit
-
     }
 
-    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
-        super.onActivityResult(requestCode, resultCode, data)
+    private fun cropWithContour(mat: Mat, matPoint: MatOfPoint): Mat {
+        // WARNING imageview is rotated 90, so bitmap might not be how it's actually displayed.
+        // TESTING
+        // See https://docs.opencv.org/3.4/da/d6e/tutorial_py_geometric_transformations.html,
+        // https://docs.opencv.org/3.4/da/d54/group__imgproc__transform.html#gaf73673a7e8e18ec6963e3774e6a94b87
+        // https://github.com/KMKnation/Four-Point-Invoice-Transform-with-OpenCV/blob/f012c16b7508e6141a1c3a77c24680198bf1c174/four_point_object_extractor.py#L8
+        // https://www.pyimagesearch.com/2014/08/25/4-point-opencv-getperspective-transform-example/
 
-//        if (resultCode == Activity.RESULT_OK) {
-//            val imageBitmap = data?.extras?.get("data") as Bitmap
-//
-//            // TODO start new fragment with 4 point transform.
-//
-//
-//
-//
-//            // https://www.pyimagesearch.com/2014/09/01/build-kick-ass-mobile-document-scanner-just-5-minutes/
-//            // Don't trust immutable yet...
-//            // TODO compress into 1 file that constantly replaces itself
-//            var testMat: Mat = Mat()
-//            var testGreyMat: Mat = Mat()
-//            var testGreyGaussianMat: Mat = Mat()
-//            var testGreyGaussianEdgeMat: Mat = Mat()
-//
-//            Utils.bitmapToMat(imageBitmap, testMat)
-//            // Greyscale
-//            Imgproc.cvtColor(testMat, testGreyMat, Imgproc.COLOR_BGR2GRAY)
-//            Imgproc.GaussianBlur(testGreyMat, testGreyGaussianMat, Size(5.0, 5.0), 0.0)
-//            Imgproc.Canny(testGreyGaussianMat, testGreyGaussianEdgeMat, 75.0, 75.0*3)
-//
-//
-//            var matPointList: MutableList<MatOfPoint> = ArrayList()
-//            var contourHierarchyMat = Mat()
-//            // See https://docs.opencv.org/3.4/d4/d73/tutorial_py_contours_begin.html
-//            Imgproc.findContours(testGreyGaussianEdgeMat, matPointList, contourHierarchyMat, Imgproc.RETR_EXTERNAL, Imgproc.CHAIN_APPROX_SIMPLE)
-//
-//            // Convert to MatOfPoint2f https://stackoverflow.com/questions/11273588/how-to-convert-matofpoint-to-matofpoint2f-in-opencv-java-api
-//            var newList: MutableList<MatOfPoint2f> = ArrayList()
-//            for (v in matPointList) {
-//                val d = MatOfPoint2f()
-//                v.convertTo(d, CvType.CV_32F)
-//                newList.add(d)
-//            }
-//
-//            newList.sortByDescending { v -> Imgproc.contourArea(v) }
-//
-//            var newoneList: MutableList<MatOfPoint> = ArrayList()
-////            val d = MatOfPoint()
-////            newList.get(0).convertTo(d, CvType.CV_32S)
-////            newoneList.add(d)
-//            for (v in newList) {
-//                val p = Imgproc.arcLength(v, true)
-//                val ap = MatOfPoint2f()
-//                Imgproc.approxPolyDP(v, ap, 0.02 * p, true)
-//                Log.d("test", ap.size().toString())
-//                if (ap.height() == 4) {
-//                    val d = MatOfPoint()
-//                    ap.convertTo(d, CvType.CV_32S)
-//                    newoneList.add(d)
-//                    break
-//                }
-//            }
-//
-//           Log.d("test", newoneList[0].toString())
-//
-//            Imgproc.drawContours(testMat, newoneList, -1, Scalar(0.0, 0.0, 255.0))
-//
-//
-////            var testBit: Bitmap = Bitmap.createBitmap(imageBitmap.width, imageBitmap.height, imageBitmap.config)
-////            Utils.matToBitmap(testMat, testBit)
-////            cameraImageDisplay.setImageBitmap(testBit)
-//
-//
-//        }
+        val transformTo: Mat = Mat(4,1, CvType.CV_32FC2)
+        transformTo.put(0, 0, 0.0, 0.0, 0.0, 400.0, 400.0, 0.0, 400.0, 400.0)
+
+        val transformFrom: Mat = Mat(4, 1, CvType.CV_32FC2)
+        transformFrom.put(0,0, 0.0, 0.0, 0.0, 100.0, 100.0, 0.0, 100.0, 100.0)
+        for (x in matPoint.toArray()) {// Testing.
+            Imgproc.circle(mat, x, 3, Scalar(0.0, 255.0, 0.0), 3)
+            Imgproc.putText(mat, "${x.x}, ${x.y}", x, Imgproc.FONT_HERSHEY_SIMPLEX, 1.0, Scalar(0.0, 255.0, 255.0))
+        }
+        val transform = Imgproc.getPerspectiveTransform(transformFrom, transformTo)
+        val newMat: Mat = Mat(4, 1, CvType.CV_32S)
+        Imgproc.warpPerspective(mat, newMat, transform, Size(400.0,400.0))// Size doesn't work as expected.?
+        return newMat
     }
 
     companion object {
