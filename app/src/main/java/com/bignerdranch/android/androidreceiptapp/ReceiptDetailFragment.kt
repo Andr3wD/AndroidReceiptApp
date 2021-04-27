5cdcf80768ccaa9ef556349adb684c37d3a8f2e6
hlin91 <hlin91@outlook.com>
2021-04-26 22:54:09 -0700

    Adding progress with ReceiptListFragment and ReceiptDetailFragment

--- app/src/main/java/com/bignerdranch/android/androidreceiptapp/ReceiptDetailFragment.kt
+++ app/src/main/java/com/bignerdranch/android/androidreceiptapp/ReceiptDetailFragment.kt
@@ -4,41 +4,114 @@ import android.content.Context
 import android.content.Intent
 import android.os.Bundle
 import android.util.Log
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
-import java.util.*
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProviders
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 
-private const val TAG = "ReceiptDetailActivity"
-private const val EXTRA_RECEIPT_ID = "com.bignerdranch.android.androidreceiptapp.receipt_id"
+private const val TAG = "ReceiptDetailFragment"
 
-class ReceiptDetailActivity : AppCompatActivity() {
+class ReceiptDetailFragment : Fragment() {
 
-    private var receipt_id: String? = null
+    private lateinit var titleTextView: TextView
+    private lateinit var dateTextView: TextView
+    private lateinit var totalTextView: TextView
+    private lateinit var entryRecyclerView: RecyclerView
+    private var adapter: EntryAdapter? = EntryAdapter(emptyList())
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.fragment_receipt_detail)
+    private val receiptDetailViewModel: ReceiptDetailViewModel by lazy {
+        ViewModelProviders.of(this).get(ReceiptDetailViewModel::class.java)
+    }
 
-        receipt_id = intent.getStringExtra(EXTRA_RECEIPT_ID)
-        if (receipt_id == null) {
-            Log.w(TAG, "Null receipt ID found in intent")
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        val view = inflater.inflate(R.layout.fragment_receipt_detail, container, false)
+
+        entryRecyclerView = view.findViewById(R.id.entry_recycler_view) as RecyclerView
+        entryRecyclerView.layoutManager = LinearLayoutManager(context)
+        entryRecyclerView.adapter = adapter
+
+        titleTextView = view.findViewById(R.id.detail_receipt_title)
+        dateTextView = view.findViewById(R.id.detail_receipt_date)
+        totalTextView = view.findViewById(R.id.detail_receipt_total)
+
+        return view
+    }
+
+    private fun updateUI(entries: List<ReceiptEntry>) {
+        adapter = EntryAdapter(entries)
+        entryRecyclerView.adapter = adapter
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        receiptDetailViewModel.entriesLiveData.observe(
+            viewLifecycleOwner,
+            Observer { entries ->
+                entries?.let {
+                    Log.i(TAG, "Got entries ${entries.size}")
+                    updateUI(entries)
+                }
+            }
+        )
+        receiptDetailViewModel.receiptLiveData.observe(
+            viewLifecycleOwner,
+            Observer { receipt ->
+                receipt?.let {
+                    titleTextView.text = receipt.Title
+                    dateTextView.text = receipt.Date.toString()
+                    totalTextView.text = receipt.TotalSpent.toString()
+                }
+            }
+        )
+    }
+
+    private inner class EntryHolder(view: View) : RecyclerView.ViewHolder(view),
+        View.OnClickListener {
+
+        private lateinit var entry: ReceiptEntry
+
+        private val nameTextView: TextView = itemView.findViewById(R.id.entry_name)
+        private val priceTextView: TextView = itemView.findViewById(R.id.entry_price)
+
+        init {
+            itemView.setOnClickListener(this)
         }
-        val currentFragment =
-                supportFragmentManager.findFragmentById(R.id.detail_entry_list_fragment)
 
-        if (currentFragment == null) {
-            val fragment = ReceiptEntryListFragment.newInstance(UUID.fromString(receipt_id))
-            supportFragmentManager
-                    .beginTransaction()
-                    .add(R.id.detail_entry_list_fragment, fragment)
-                    .commit()
+        fun bind(entry: ReceiptEntry) {
+            this.entry = entry
+            nameTextView.text = this.entry.Name
+            priceTextView.text = this.entry.Price.toString()
+        }
+
+        override fun onClick(v: View) {
+            return
         }
     }
 
-    companion object {
-        fun newIntent(packageContext: Context, receiptId: String): Intent {
-            return Intent(packageContext, ReceiptDetailActivity::class.java).apply {
-                putExtra(EXTRA_RECEIPT_ID, receiptId)
-            }
+    private inner class EntryAdapter(var entries: List<ReceiptEntry>)
+        : RecyclerView.Adapter<EntryHolder>() {
+
+        override fun onCreateViewHolder(parent: ViewGroup, viewType: Int)
+        : EntryHolder {
+            val view = layoutInflater.inflate(R.layout.list_item_entry, parent, false)
+            return EntryHolder(view)
+        }
+
+        override fun getItemCount() = entries.size
+
+        override fun onBindViewHolder(holder: EntryHolder, position: Int) {
+            val entry = entries[position]
+            holder.bind(entry)
         }
     }
 }
